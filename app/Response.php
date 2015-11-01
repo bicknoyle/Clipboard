9cd0a7473ec10d07d12e4b38f46945aafed7933b
vagrant <nsboyle@gmail.com>
2015-11-01 03:30:43 +0000

    Dithed repositories and bundle more logic into models

--- app/Response.php
+++ app/Response.php
@@ -2,7 +2,11 @@
 
 namespace App;
 
+use App\Answer;
+use App\Question;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Response extends Model
 {
@@ -12,6 +16,23 @@ class Response extends Model
      * @var array
      */
     protected $fillable = ['survey_id', 'ip'];
+
+    public function addAnswer(Answer $answer)
+    {
+        $this->answers()->save($answer);
+    }
+
+    public function answerQuestion(Question $question, $value)
+    {
+        $this->addAnswer(new Answer(['question_id' => $question->id, 'value' => $value]));
+    }
+
+    public function answerQuestions(EloquentCollection $questions, Collection $values)
+    {
+        foreach($questions as $question) {
+            $this->answerQuestion($question, $values->get($question->field, ''));
+        }
+    }
 
     public function answers()
     {
